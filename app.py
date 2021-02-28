33aa099de347206d6c27f511ae841f2154f66ab5
Jack Grahl <jack.grahl@gmail.com>
2021-02-28 16:21:39 +0000

    Add file to serve stuff and do honeycomb.

---
+++ app.py
@@ -0,0 +1,21 @@
+import beeline
+
+from beeline.middleware.flask import HoneyMiddleware
+from flask import Flask
+
+from local_secrets import honeycomb_api_key
+
+beeline.init(
+    writekey=honeycomb_api_key,
+    dataset="taas",
+    service_name="taas",
+    debug=True,
+)
+
+app = Flask(__name__)
+HoneyMiddleware(app, db_events=True)
+
+@app.route("/hello")
+def hello_world():
+    return "Hello, world!"
+
